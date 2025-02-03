9ed06b16218f0a0ca6287d9542fa12fb82d6da26
Ashen Jayarathne <ashenjayarathnedev@gmail.com>
2025-02-03 12:43:06 +0530

    Configure backend instance identifier

--- server/server.js
+++ server/server.js
@@ -1,9 +1,13 @@
+const os = require("os");
 const express = require("express");
 const app = express();
 const port = 3000;
 
+// os.hostname() = container ID
+const instance = process.env.INSTANCE || os.hostname();
+
 app.get("/", (req, res) => {
-  res.send(`Hello, World`);
+  res.send(`Hello from instance: ${instance}`);
 });
 
 app.listen(port, () => {
